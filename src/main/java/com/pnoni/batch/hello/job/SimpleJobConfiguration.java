65dd32d672fd77010a04ac73cda65175fb04b99b
Julio Kim <julio@pnoni.com>
2023-07-11 01:07:25 +0900

    * JobParametersValidator * 2.1.0 version update

--- src/main/java/com/pnoni/batch/hello/job/SimpleJobConfiguration.java
+++ src/main/java/com/pnoni/batch/hello/job/SimpleJobConfiguration.java
@@ -2,13 +2,15 @@ package com.pnoni.batch.hello.job;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,6 +24,12 @@ public class SimpleJobConfiguration {
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1())
                 .next(simpleStep2())
+//                .validator(jobParameters -> {
+//                    if (Optional.ofNullable(jobParameters).isEmpty() ||
+//                                Optional.ofNullable(jobParameters.getString("name")).isEmpty())
+//                        throw new JobParametersInvalidException("'name' parameters is not founded");
+//                })
+                .validator(new DefaultJobParametersValidator(new String[] {"name"}, new String[] {"count"}))
                 .build();
     }
 
