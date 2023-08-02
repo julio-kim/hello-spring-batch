d726013ac592a595bd1e46e9580d48d0adbf6bb8
Julio Kim <julio@pnoni.com>
2023-08-02 20:49:57 +0900

    * FlatFileItemWriter - formatted * 3.12.0 version update

--- src/main/java/com/pnoni/batch/hello/job/FlatFileFixedJobConfiguration.java
+++ src/main/java/com/pnoni/batch/hello/job/FlatFileFixedJobConfiguration.java
@@ -7,12 +7,15 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.transform.Range;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 @Slf4j
 @Configuration
@@ -32,27 +35,35 @@ public class FlatFileFixedJobConfiguration {
     public Step flatFileFixedStep() {
         return stepBuilderFactory.get("flatFileFixedStep")
                 .<BatchLog, BatchLog>chunk(5)
-                .reader(filatFileFixedItemReader())
-                .writer(items -> log.info("Items: {}", items))
+                .reader(flatFileFixedItemReader())
+                .writer(flatFileFormatWriter())
                 .build();
     }
 
     @Bean
-    public ItemReader<BatchLog> filatFileFixedItemReader() {
+    public ItemReader<BatchLog> flatFileFixedItemReader() {
         return new FlatFileItemReaderBuilder<BatchLog>()
                 .name("flatFileFixed")
                 .resource(new ClassPathResource("template/batch.log"))
                 .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
                 .targetType(BatchLog.class)
                 .fixedLength()
-//                .strict(false)
                 .addColumns(new Range(1, 24))
                 .addColumns(new Range(26, 30))
                 .addColumns(new Range(42, 57))
                 .addColumns(new Range(59, 98))
                 .addColumns(new Range(102))
-//                .addColumns(new Range(150))
                 .names("occurTime", "level", "threadName", "clazz", "message")
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<BatchLog> flatFileFormatWriter() {
+        return new FlatFileItemWriterBuilder<BatchLog>()
+                .name("flatFileFormatWriter")
+                .resource(new FileSystemResource("target/batch-new.log"))
+                .formatted().format("[%5s] %s")
+                .names("level", "message")
                 .build();
     }
 }
