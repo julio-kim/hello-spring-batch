fd233fe32b4867f4650a8cf9e2dc04b3de442c2b
Julio Kim <julio@pnoni.com>
2023-08-02 20:58:33 +0900

    * JsonFileItemWriter * 3.13.0 version update

--- src/main/java/com/pnoni/batch/hello/job/JsonJobConfiguration.java
+++ src/main/java/com/pnoni/batch/hello/job/JsonJobConfiguration.java
@@ -8,11 +8,15 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
 import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 @Slf4j
 @Configuration
@@ -34,7 +38,7 @@ public class JsonJobConfiguration {
         return stepBuilderFactory.get("jsonStep")
                 .<Employee, Employee>chunk(5)
                 .reader(jsonItemReader())
-                .writer(items -> log.info("Items : {}", items))
+                .writer(jsonItemWriter())
                 .build();
     }
 
@@ -44,6 +48,15 @@ public class JsonJobConfiguration {
                 .name("jsonReader")
                 .resource(new ClassPathResource("template/employee.json"))
                 .jsonObjectReader(new JacksonJsonObjectReader<>(Employee.class))
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Employee> jsonItemWriter() {
+        return new JsonFileItemWriterBuilder<Employee>()
+                .name("jsonFileWriter")
+                .resource(new FileSystemResource("target/employee-new.json"))
+                .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
                 .build();
     }
 }
